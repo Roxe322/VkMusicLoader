a6d52803aaa171a117217327edf861cea477ea70
Roxe322 <kolomatskiy@bk.ru>
2019-01-14 21:18:28 +0300

    fixed VK_ID passing for loader

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from downloader import Downloader
 from playlist_creator import PlaylistMaker
 
-pm = PlaylistMaker()
 
-file_name = pm.create_playlist_file(9693122)
+def main() -> None:
+    pm = PlaylistMaker()
+
+    user_id = input("Type your VK_ID. Audios shouldn't be private: ")
+
+    try:
+        user_id = int(user_id)
+    except ValueError:
+        print('Invalid VK_ID')
+        return
+
+    file_name = pm.create_playlist_file(user_id)
+
+    dl = Downloader('music', file_name, 30)
+
+    dl.run()
 
 
-dl = Downloader('music', file_name, 30)
-
-dl.run()
+if __name__ == '__main__':
+    main()
